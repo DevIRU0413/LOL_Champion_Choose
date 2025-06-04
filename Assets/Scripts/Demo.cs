73cca8a648135f08858329b9ac769f590bfa99de
DevIRU <deviru0413@gmail.com>
2025-06-05 08:04:23 +0900

    [feat] 룰렛 관련 기능 및 프리팹
    - 룰렛 실행기
    - 룰렛 기능
    - 룰렛 씬
    - 룰렛 필요 피스, 라인 프리팹

---
+++ Assets/Scripts/Demo.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Demo : MonoBehaviour
+{
+
+    [SerializeField] private Roulette roulette;
+    [SerializeField] Button btnSpin;
+
+    private void Awake()
+    {
+        btnSpin.onClick.AddListener(() =>
+        {
+            btnSpin.interactable =false;
+            roulette.Spin(EndOfSpin);
+
+        });
+    }
+
+    private void EndOfSpin(RoulettePieceData seletedData)
+    {
+        btnSpin.interactable = true;
+        Debug.Log($"{seletedData.data.name}");
+    }
+}
---
+++ Assets/Scripts/Tool/Roulette.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Roulette : MonoBehaviour
+{
+    [SerializeField] private Transform piecePrefab;
+    [SerializeField] private Transform linePrefab;
+
+    [SerializeField] private Transform pieceParent;
+    [SerializeField] private Transform lineParent;
+
+    [SerializeField] private RoulettePieceData[] roulettePieceData;
+
+    [SerializeField] private int spinDuration;
+    [SerializeField] private Transform spinningRoulette;
+    [SerializeField] private AnimationCurve spinningCurve;
+
+    private float pieceAngle;
+    private float halfPieceAngle;
+    private float halfPieceAngleWithPaddings;
+
+    private float accumulatedWeight;
+
+    private bool isSpinning = false;
+    private int selectedIndex = 0;
+
+    private void Awake()
+    {
+        pieceAngle = 360 / roulettePieceData.Length;
+        halfPieceAngle = pieceAngle * 0.5f;
+        halfPieceAngleWithPaddings = halfPieceAngle - (halfPieceAngle * 0.25f);
+
+        SpawnPiecesAndLines();
+        CalculateWeightAndIndices();
+        // GetRandomIndex();
+    }
+
+    private void SpawnPiecesAndLines()
+    {
+        for (int i = 0; i < roulettePieceData.Length; ++i)
+        {
+            Transform piece = Instantiate(piecePrefab, pieceParent.position, Quaternion.identity, pieceParent);
+            piece.GetComponent<RoulettePiece>().Setup(roulettePieceData[i]);
+            piece.RotateAround(pieceParent.position, Vector3.back, (pieceAngle * i));
+
+            Transform line = Instantiate(linePrefab, lineParent.position, Quaternion.identity, lineParent);
+            line.RotateAround(lineParent.position, Vector3.back, (pieceAngle * i) + halfPieceAngle);
+        }
+    }
+
+    private void CalculateWeightAndIndices()
+    {
+        for (int i = 0; i < roulettePieceData.Length; ++i)
+        {
+            if (roulettePieceData[i].chane <= 0)
+            {
+                roulettePieceData[i].chane = 1;
+            }
+
+            accumulatedWeight += roulettePieceData[i].chane;
+            roulettePieceData[i].weight = accumulatedWeight;
+        }
+    }
+
+    private int GetRandomIndex()
+    {
+        float weight = Random.Range(0.0f, accumulatedWeight);
+        Debug.Log($"weight: {weight}");
+        for(int i = 0; i < roulettePieceData.Length; i++)
+        {
+            if (roulettePieceData[i].weight > weight)
+            {
+                return i;
+            }
+        }
+
+        return 0;
+    }
+
+    public void Spin(UnityAction<RoulettePieceData> action = null)
+    {
+        if (isSpinning) return;
+
+        selectedIndex = GetRandomIndex();
+
+        float angle = pieceAngle * selectedIndex;
+
+        float leftOffset = (angle - halfPieceAngleWithPaddings) % 360;
+        float rightOffset = (angle + halfPieceAngleWithPaddings) % 360;
+        float randomAngle = Random.Range(leftOffset, rightOffset);
+
+        int rotateSpeed = 2;
+        float targetAngle = (randomAngle + 360 * spinDuration * rotateSpeed);
+
+        isSpinning = true;
+        StartCoroutine(OnSpin(targetAngle, action));
+    }
+
+    private IEnumerator OnSpin(float end, UnityAction<RoulettePieceData> action)
+    {
+        float current = 0;
+        float percent = 0;
+
+        while(percent < 1)
+        {
+            current += Time.deltaTime;
+            percent = current / spinDuration;
+
+            float z = Mathf.Lerp(0, end, spinningCurve.Evaluate(percent));
+            spinningRoulette.rotation = Quaternion.Euler(0,0,z);
+            yield return null;
+        }
+
+        isSpinning = false;
+
+        if (action != null) action.Invoke(roulettePieceData[selectedIndex]);
+    }
+}
