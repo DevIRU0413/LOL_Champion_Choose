bdbc9d051e618cc1e93f036e2b90ba395c6cb4a8
DevIRU <deviru0413@gmail.com>
2025-06-04 10:25:28 +0900

    [chore] 프로젝트 세팅 수정

---
+++ Assets/Scripts/Data/BGItemData.cs
@@ -0,0 +1,43 @@
+﻿using Scripts.Data;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BGItemData : MonoBehaviour
+{
+    private readonly string INVENTORY_ITEM_IMAGE_NAME = "Icon";
+
+    public ChampionData data ;
+    private Image m_targetImage;
+
+    public void Setting(Image targetImage, Transform parentTr, ChampionData data)
+    {
+        m_targetImage = targetImage;
+        this.gameObject.transform.parent = parentTr;
+        this.data = data;
+
+        // 현재 자신 기본 크기로 설정
+        gameObject.transform.localScale = Vector3.one;
+
+        // 자식들 가지고 와서 이이지 적용
+        var dataGoChilds = gameObject.GetComponentsInChildren<Transform>();
+        foreach (var childGo in dataGoChilds)
+        {
+            if (childGo.name == INVENTORY_ITEM_IMAGE_NAME)
+            {
+                Image image = childGo.GetComponent<Image>();
+
+                image.sprite = data.portraitSprite;
+                break;
+            }
+        }
+
+        // 이벤트 연결
+        var btnCmp = gameObject.GetComponent<Button>();
+        btnCmp.onClick.AddListener(ChangeBackGround);
+    }
+
+    public void ChangeBackGround()
+    {
+        m_targetImage.sprite = data.splashSprite;
+    }
+}
