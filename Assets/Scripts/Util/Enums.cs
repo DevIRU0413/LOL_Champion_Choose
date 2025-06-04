fad4ceeeb42c1fa39cf488d5db2655b0da659063
DevIRU <deviru0413@gmail.com>
2025-06-04 18:14:56 +0900

    [feat] Util 관련 기능 추가
    - 기본 필요 Enum 선언

---
+++ Assets/Scripts/Util/Enums.cs
@@ -0,0 +1,34 @@
+﻿namespace Scripts.Util
+{
+    public enum SceneID
+    {
+        AppInitialize,  // AppInitialize 0
+        Title,          // Title 1
+        Roulette,
+    }
+
+    // 게임 상태용
+    public enum PlayState
+    {
+        None,     // 기본 상태 (선택사항)
+        Playing,  // 게임이 진행 중
+        Paused,   // 일시 정지
+        Stopped   // 강제 멈춤
+    }
+
+    // 게임 상태용
+    public enum ManagerPriority
+    {
+        None,                   // Error
+
+        ResourceManager, // Manager
+        SceneManagerEx,
+        AudioManager,
+        UIManager,
+
+        AppInitializer,
+
+        ChampionManager,
+        MonsterManager,
+    }
+}
